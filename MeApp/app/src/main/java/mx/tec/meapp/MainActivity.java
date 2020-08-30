9534c6597e389b43e9868e18836cc2b0d29cac9e
S0mbiy <alvaradoomar55@hotmail.com>
2020-08-30 12:04:14 -0500

    H2
    Signed-off-by: S0mbiy <alvaradoomar55@hotmail.com>

--- MeApp/app/src/main/java/mx/tec/meapp/MainActivity.java
+++ MeApp/app/src/main/java/mx/tec/meapp/MainActivity.java
@@ -2,16 +2,26 @@ package mx.tec.meapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends AppCompatActivity {
     EditText input;
     Button b;
+    Boolean properties;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +29,60 @@ public class MainActivity extends AppCompatActivity {
 
         input = findViewById(R.id.nameText);
         b = findViewById(R.id.menuButton);
+        String name = readFromFile(this);
+        input.setText(name);
     }
 
     public void menu(View v){
 
-
+        if(!properties){
+            writeToFile(input.getText().toString(), this);
+        }
         Intent intentito = new Intent(this, MenuActivity.class);
 
         intentito.putExtra("name", input.getText().toString());
         startActivityForResult(intentito, 0);
 
+    }
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter out = new OutputStreamWriter(context.openFileOutput("properties.txt", Context.MODE_PRIVATE));
+            out.write(data);
+            out.close();
+            properties = true;
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream input = context.openFileInput("properties.txt");
+
+            if ( input != null ) {
+                InputStreamReader inputReader = new InputStreamReader(input);
+                BufferedReader bufferedReader = new BufferedReader(inputReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                properties = true;
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                input.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            properties = false;
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 }
